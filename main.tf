f72ef4c723d14dee1ee1a723499e0496cfd3afc7
stephane-chang <stephane.bounkong@gmail.com>
2025-02-14 22:58:02 -0500

    cpying from the solution

--- main.tf
+++ main.tf
@@ -45,32 +45,27 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [ 
-    module.blog_sg.security_group_id
-  ]
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id      = "aws_instance.blog.id"
-          port           = 80
-        }
-      }
     }
   ]
-  
+
   http_tcp_listeners = [
     {
       port               = 80
