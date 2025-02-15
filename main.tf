160dcf675d5dafc143c282e1530a31131247a951
stephane-chang <stephane.bounkong@gmail.com>
2025-02-14 23:11:19 -0500

    Bug fix

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.publics_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-  security_group      = [module.blog_sg.security_group_id]
+  security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
