abb9d696aa965e112222a9bc639f7ebeaf00cf4e
stephane-chang <stephane.bounkong@gmail.com>
2025-02-16 16:44:19 -0500

    taking from the solution

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "app_ami" {
+  data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
@@ -12,10 +12,6 @@ data "aws_ami" "app_ami" {
   }
 
   owners = ["979382823631"] # Bitnami
-}
-
-data "aws_vpc" "default" {
-  default = true
 }
 
 module "blog_vpc" {
@@ -33,20 +29,19 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name     = "blog"
-  min_size = 1
-  max_size = 2
+  name = "blog"
 
-  vpc_zone_identifier = module.blog_vpc.publics_subnets
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -85,11 +80,10 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name  = "blog"
+  version = "4.13.0"
+  name                = "blog"
 
-  vpc_id = module.blog_vpc.vpc_id
-
+  vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
